dd9712cdca9cc7282198da60539cd081cfd0336d
Ahmed-Abdullah14 <ahmed.abdullahsr16@gmail.com>
2026-03-21 11:34:53 -0600

    Updated .env.example and config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
-
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
 OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
 
@@ -12,14 +10,15 @@ SUPABASE_URL = os.getenv("SUPABASE_URL", "")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
 
 MANAGER_MODEL = os.getenv("MANAGER_MODEL", "openai/gpt-4o-mini")
-#PROFILER_MODEL = os.getenv("PROFILER_MODEL", "openai/gpt-4o-mini")   
-#CLASSIFIER_MODEL = os.getenv("CLASSIFIER_MODEL", "perplexity/sonar-pro")
-#CLUSTER_LABEL_MODEL = os.getenv("CLUSTER_LABEL_MODEL", "openai/gpt-4o")
-#CONTENT_MODEL = os.getenv("CONTENT_MODEL", "")
+PROFILER_MODEL = os.getenv("PROFILER_MODEL", "openai/gpt-5.1") 
+CLASSIFIER_MODEL = os.getenv("CLASSIFIER_MODEL", "openai/gpt-5.1")
+CLUSTER_LABEL_MODEL = os.getenv("CLUSTER_LABEL_MODEL", "openai/gpt-4o")
+CONTENT_MODEL = os.getenv("CONTENT_MODEL", "openai/gpt-4o")
 
 
-#TEXT_EMBEDDING_MODEL = "text-embedding-3-small"
-#CLIP_MODEL = "openai/clip-vit-large-patch14"
+#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")           # Used for embeddings since operrouter does not support embedding models
+TEXT_EMBEDDING_MODEL = "text-embedding-3-small"
+CLIP_MODEL = "openai/clip-vit-large-patch14"
 
 
 APP_NAME = "LumenIQ"
